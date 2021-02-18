79f65db1bc34b5f8f773e07c622eef62d80b5155
Micro User <micro@BPANTALA-5ZX0A.cisco.com>
2021-02-18 07:45:51 +0530

    1.0

---
+++ lambda/src/RotateAccessKey.py
@@ -0,0 +1,341 @@
+import boto3
+from datetime import datetime
+import dateutil.tz
+import json
+import ast
+import re
+import time
+
+BUILD_VERSION = '@@buildversion'
+
+AWS_REGION = '@@deploymentregion'
+SES_REGION = '@@sesregion'
+AWS_ACCOUNT_NAME = '@@awsaccountname'
+AWS_ACCOUNT_ID = '@@awsaccountid'
+
+SKIP_USERNAMES = '@@skipusernames'
+try:
+    #package.json via iam.skip_usernames
+    SKIP_USERNAMES = ast.literal_eval(SKIP_USERNAMES)
+except:
+    #commandline via --skipusers parameter
+    SKIP_USERNAMES = SKIP_USERNAMES.split(",")
+
+EMAIL_FROM = '@@emailfrom'
+EMAIL_ADMIN_ENABLED = ast.literal_eval('@@emailadmin')
+EMAIL_ADMIN_TO = '@@emailadminto'
+EMAIL_USER_CONFIG = ast.literal_eval('@@emailuser')
+
+EMAIL_REGEX = re.compile(r'[^@]+@[^@]+\.[^@]+')
+
+# Length of mask over the IAM Access Key
+MASK_ACCESS_KEY_LENGTH = ast.literal_eval('@@maskaccesskeylength')
+
+# First email warning
+FIRST_WARNING_NUM_DAYS = @@first_warning_num_days
+FIRST_WARNING_MESSAGE = '@@first_warning_message'
+# Last email warning
+LAST_WARNING_NUM_DAYS = @@last_warning_num_days
+LAST_WARNING_MESSAGE = '@@last_warning_message'
+
+# Max AGE days of key after which it is considered EXPIRED (deactivated)
+KEY_MAX_AGE_IN_DAYS = @@key_max_age_in_days
+KEY_EXPIRED_MESSAGE = '@@key_expired_message'
+
+KEY_YOUNG_MESSAGE = '@@key_young_message'
+
+# ==========================================================
+
+# Character length of an IAM Access Key
+ACCESS_KEY_LENGTH = 20
+KEY_STATE_ACTIVE = "Active"
+KEY_STATE_INACTIVE = "Inactive"
+
+# ==========================================================
+
+#check to see if the MASK_ACCESS_KEY_LENGTH has been misconfigured
+if MASK_ACCESS_KEY_LENGTH > ACCESS_KEY_LENGTH:
+    MASK_ACCESS_KEY_LENGTH = 16
+
+# ==========================================================
+def tzutc():
+    return dateutil.tz.tzutc()
+
+
+def key_age(key_created_date):
+    tz_info = key_created_date.tzinfo
+    age = datetime.now(tz_info) - key_created_date
+
+    #print ('key age %s' % age)
+
+    key_age_str = str(age)
+    if 'days' not in key_age_str:
+        return 0
+
+    days = int(key_age_str.split(',')[0].split(' ')[0])
+
+    return days
+
+def send_admin_invaliduseremailaddress_email(userlist):
+    subject = f'AWS IAM Access Key Rotation for Account: {AWS_ACCOUNT_NAME} / {AWS_ACCOUNT_ID} - Detected Missing/Invalid Email for Users Report'
+    body = f'The following report contains a list of users who do NOT appear to have a valid email address. Please review username and tags for each user within IAM.\n\n{userlist}'
+    send_admin_email(subject, body)
+
+def send_admin_deactivate_email(userlist):
+    subject = f'AWS IAM Access Key Rotation for Account: {AWS_ACCOUNT_NAME} / {AWS_ACCOUNT_ID} - Deactivation of Access Key for Users Report'
+    body = f'The following report contains a list of users who have had their access key automatically deactivated due to it being too old.\n\n{userlist}'
+    send_admin_email(subject, body)
+
+def send_admin_completion_email(finished, deactivated_report):
+    user_list = '<table cellspacing="4" cellpadding="4" border="0">'
+    for user in deactivated_report["users"]:
+        if len(user['keys']) > 0:
+            user_list += '<tr>'
+            user_list += '<td valign="top">'
+            user_list += f'User <b>{user["username"]}</b> has keys in the following state:'
+            user_list += '</td>'
+            user_list += '<td valign="top">'
+            user_list += '<table cellspacing="0" cellpadding="0" border="0">'
+            for key in user["keys"]:
+                user_list += '<tr><td>'
+                user_list += f'{key["accesskeyid"]}, age {key["age"]}, {key["state"]}'
+                user_list += '</td></tr>'
+            user_list += '</table>'
+            user_list += '</td>'
+            user_list += '</tr>'
+    user_list += '</table>'
+
+    subject = f'AWS IAM Access Key Rotation for Account: {AWS_ACCOUNT_NAME} / {AWS_ACCOUNT_ID} - Completion Report'
+
+    body = f"""<html>
+    <head></head>
+    <body>
+    <h1>Deactivation Report</h1>
+    <p>AWS IAM Access Key Rotation Lambda Function (cron job) finished successfully.</p>
+    <hr>
+    {user_list}
+    </body>
+    </html>"""
+
+    send_admin_email(subject, body)
+
+def send_admin_email(subject, body):
+    client = boto3.client('ses', region_name=SES_REGION)
+    response = client.send_email(
+        Source=EMAIL_FROM,
+        Destination={
+            'ToAddresses': [EMAIL_ADMIN_TO]
+        },
+        Message={
+            'Subject': {
+                'Charset': 'UTF-8',
+                'Data': subject
+            },
+            'Body': {
+                'Html': {
+                    'Charset': 'UTF-8',
+                    'Data': body
+                }
+            }
+        })
+
+#Will send email containing one of the following messages:
+#Your AWS IAM Access Key (****************34MI) is due to expire in 1 week (7 days) - please rotate.
+#Your AWS IAM Access Key (****************34MI) is due to expire in 1 day (tomorrow) - please rotate.
+#Your AWS IAM Access Key (****************34MI) is now EXPIRED! Changing key to INACTIVE state - please rotate.
+def send_user_email(email_to, key, message):
+    try:
+        if not email_to or not EMAIL_REGEX.match(email_to):
+            return
+
+        client = boto3.client('ses', region_name=SES_REGION)
+        response = client.send_email(
+            Source=EMAIL_FROM,
+            Destination={
+                'ToAddresses': [email_to]
+            },
+            Message={
+                'Subject': {
+                    'Data': f'AWS IAM Access Key Rotation: {AWS_ACCOUNT_NAME} / {AWS_ACCOUNT_ID}'
+                },
+                'Body': {
+                    'Html': {
+                    'Data': f'Your AWS IAM Access Key {key} {message}.'
+                    }
+                }
+            })
+    except:
+        pass
+def mask_access_key(access_key):
+    return access_key[-(ACCESS_KEY_LENGTH-MASK_ACCESS_KEY_LENGTH):].rjust(len(access_key), "*")
+
+
+def lambda_handler(event, context):
+    print ('*****************************')
+    print (f'RotateAccessKey v{BUILD_VERSION}: starting...')
+    print ("*****************************")
+    # Connect to AWS APIs
+    client = boto3.client('iam')
+
+    users = {}
+    data = client.list_users()
+    #print (data)
+
+    userindex = 0
+
+    for user in data['Users']:
+        userid = user['UserId']
+        username = user['UserName']    
+        
+        usertags = client.list_user_tags(UserName=username)
+
+        users[userid] = { "username": username, "tags": usertags}
+
+    users_report = []
+    users_list_first_warning = []
+    users_list_last_warning = [] 
+    users_list_keys_deactivated = []
+    users_list_email_tag_invalid = []
+
+
+    email_user_enabled = False
+    try:
+        email_user_enabled = EMAIL_USER_CONFIG["enabled"]
+    except:
+        pass
+
+    for user in users:
+        email_user_address = None
+
+        userindex += 1
+        user_keys = []
+
+        #print ('---------------------')
+        #print ('userindex %s' % userindex)
+        #print ('user %s' % user)
+        username = users[user]["username"]
+        usertags = users[user]["tags"]
+        #print ('username %s' % username)
+        #print ('usertags %s' % usertags)
+
+        # check to see if the current user is a special service account
+        if username in SKIP_USERNAMES:
+           #print(f'detected special username (configured service account etc.) {username}, key rotation skipped for this account...')
+            continue
+
+        # determine if USER based email address is configured
+        # can be either username based or tag based
+        user_email_address = None
+        if email_user_enabled:
+            try:
+        #       if EMAIL_USER_CONFIG["emailaddressconfig"]["type"] == "username":
+        #           if EMAIL_REGEX.match(username):
+        #                email_user_address = username
+                if EMAIL_USER_CONFIG["emailaddressconfig"]["type"] == "tag":
+                    validuseremailaddress = False
+                    for tag in usertags["Tags"]:
+                        if tag["Key"] == EMAIL_USER_CONFIG["emailaddressconfig"]["tagname"]:
+                            tag_emailaddress = tag["Value"]
+                            if EMAIL_REGEX.match(tag_emailaddress):
+                                user_email_address = tag_emailaddress
+                                validuseremailaddress = True
+                                break
+                    if not validuseremailaddress:
+                        users_list_email_tag_invalid.append(username)                        
+
+
+            except Exception:
+                pass
+
+        #if EMAIL_ADMIN_ENABLED and EMAIL_USER_CONFIG["emailaddressconfig"]["reportmissingtag"] and not validuseremailaddress:
+            #send_admin_invaliduseremailaddress_email(username, EMAIL_USER_CONFIG["emailaddressconfig"]["tagname"])
+
+        access_keys = client.list_access_keys(UserName=username)['AccessKeyMetadata']
+        for access_key in access_keys:
+        #    print (access_key)
+            access_key_id = access_key['AccessKeyId']
+
+            masked_access_key_id = mask_access_key(access_key_id)
+
+        #    print ('AccessKeyId %s' % masked_access_key_id)
+
+            existing_key_status = access_key['Status']
+        #    print (existing_key_status)
+
+            key_created_date = access_key['CreateDate']
+        #    print ('key_created_date %s' % key_created_date)
+
+            age = key_age(key_created_date)
+        #    print ('age %s' % age)
+
+            # we only need to examine the currently Active and about to expire keys
+            if existing_key_status == "Inactive":
+                key_state = 'key is already in an INACTIVE state'
+                key_info = {'accesskeyid': masked_access_key_id, 'age': age, 'state': key_state, 'changed': False}
+                user_keys.append(key_info)
+                continue
+
+            key_state = ''
+            key_state_changed = False
+            if age < FIRST_WARNING_NUM_DAYS:
+                key_state = KEY_YOUNG_MESSAGE
+            elif age == FIRST_WARNING_NUM_DAYS:
+                key_state = FIRST_WARNING_MESSAGE
+                users_list_first_warning.append(username)
+                if email_user_enabled and user_email_address:                    
+                    send_user_email(user_email_address, masked_access_key_id, FIRST_WARNING_MESSAGE)
+            elif age == LAST_WARNING_NUM_DAYS:
+                key_state = LAST_WARNING_MESSAGE
+                users_list_last_warning.append(username)
+                if email_user_enabled and user_email_address:
+                    send_user_email(user_email_address, masked_access_key_id, LAST_WARNING_MESSAGE)
+            elif age >= KEY_MAX_AGE_IN_DAYS:
+                key_state = KEY_EXPIRED_MESSAGE
+                users_list_keys_deactivated.append(username)
+                client.update_access_key(UserName=username, AccessKeyId=access_key_id, Status=KEY_STATE_INACTIVE)
+                if email_user_enabled and user_email_address:
+                    send_user_email(user_email_address, masked_access_key_id, KEY_EXPIRED_MESSAGE)
+                
+                key_state_changed = True
+
+        #    print ('key_state %s' % key_state)
+
+            key_info = {'accesskeyid': masked_access_key_id, 'age': age, 'state': key_state, 'changed': key_state_changed}
+            user_keys.append(key_info)
+
+        #user_info_with_username = {'userid': userindex, 'username': username, 'keys': user_keys}
+        #user_info_without_username = {'userid': userindex, 'keys': user_keys}
+
+        #users_report1.append(user_info_with_username)
+        #users_report2.append(user_info_without_username)
+        users_report.append({'userid': userindex, 'username': username, 'keys': user_keys})
+
+    finished = str(datetime.now())
+    deactivated_report = {'reportdate': finished, 'users': users_report}
+#    print ('deactivated_report1 %s ' % deactivated_report1)
+
+    if EMAIL_ADMIN_ENABLED:
+        try:
+            send_admin_completion_email(finished, deactivated_report)
+            if len(users_list_keys_deactivated) > 0:
+                send_admin_deactivate_email(users_list_keys_deactivated)
+            if EMAIL_USER_CONFIG["emailaddressconfig"]["reportmissingtag"] and len(users_list_email_tag_invalid) > 0:
+                send_admin_invaliduseremailaddress_email(users_list_email_tag_invalid)
+        except:
+            pass
+        
+    print(f'List of usernames notified with first warning: {users_list_first_warning}')
+    print(f'List of usernames notified with last warning: {users_list_last_warning}')
+    print(f'List of usernames whose keys were deactivated today: {users_list_keys_deactivated}')
+    print(f'List of usernames who dont have a valid email tag: {users_list_email_tag_invalid}')
+
+    print ('*****************************')
+    print(f'Completed (v{BUILD_VERSION}): {finished}')
+    print ('*****************************')
+    return deactivated_report
+
+#if __name__ == "__main__":
+#    event = 1
+#    context = 1
+#    lambda_handler(event, context)
+
---
+++ readme.md
@@ -0,0 +1,282 @@
+# AWS Lambda - IAM Access Key Disabler
+
+![Image of KeyIcon](/docs/images/GitHubTepapaKeyIcon.jpg)
+
+The AWS Key disabler is a Lambda Function that disables AWS IAM User Access Keys after a set amount of time in order to reduce the risk associated with old access keys.
+
+## AWS Lambda Architecture
+
+![Image of Arch](/docs/images/GitHubTepapaLambda.png)
+
+## SysOps Output for EndUser
+
+![Image of iPhoneEmail](/docs/images/GitHubTepapaOutput.png)
+
+## Developer Toolchain
+
+![Image of Toolchain](/docs/images/GitHubTepapaToolchain.png)
+
+## Features
+
+Can be configued to:
+* Email a warning message to any IAM user account whose access keys are about to expire or have been expired, refer to the `email.user` section within the `/grunt/package.json` build configuration file. The user's email address can be derived either from their **username**, or by specifying a named tag on the IAM user account:
+
+**Tag** based email address config extract:
+```
+    "email": {
+      "from": "no-reply@example.com",
+      "admin": {
+        "enabled": "False",
+        "to": "admin@example.com"
+      },
+      "user": {
+        "enabled": "True",
+        "emailaddressconfig": {
+          "type": "tag",
+          "tagname": "email",
+          "reportmissingtag": "True"
+        }
+      }
+    },
+```
+
+**Username** based email address config extract:
+```
+    "email": {
+      "from": "no-reply@example.com",
+      "admin": {
+        "enabled": "False",
+        "to": "admin@example.com"
+      },
+      "user": {
+        "enabled": "True",
+        "emailaddressconfig": {
+          "type": "username"
+        }
+      }
+    },
+```
+
+* Email a report containing the output (json) for a single key scan to a single defined administrator account, refer to the `email.admin` section within the `/grunt/package.json` build configuration file.
+
+Note: IAM Access Keys are only **disabled**, not deleted nor replaced
+
+## Prerequisites
+
+This script requires the following tools to run.
+* NPM installed https://nodejs.org/en/ - tested with version `6.14.8`
+* Gruntjs installed http://gruntjs.com/ - tested with version `grunt-cli v1.3.2`
+* AWSCLI commandline tool installed https://aws.amazon.com/cli/ - tested with version `aws-cli/2.0.58`
+
+**Note**: Setup assumes that you have an existing AWS account with SES (email) enabled. SES usage requires that you have a domain verified and sandbox mode removed.
+
+## Installation Instructions
+
+These instructions are for OSX. Your mileage may vary on Windows and other \*nix.
+
+Before you start make sure that your AWSCLI configuration has been correctly setup with the right credentials and that it can authenitcate into your AWS account. Run the following command to check who it is authenticating as:
+
+```
+aws iam get-user
+``` 
+
+IAM permissions must be granted to the account which is used to perform the deployment (`grunt deployLambda`). The required permissions must at least allow the following AWS CLI commands to run, the fullset used for deployment and setup:
+
+```
+aws iam list-policies
+aws iam create-policy
+aws iam list-roles
+aws iam create-role
+aws iam list-attached-role-policies
+aws iam attach-role-policy
+aws lambda list-functions
+aws lambda delete-function
+aws iam get-role
+aws lambda create-function
+aws events list-rules
+aws events put-rule
+aws lambda add-permission
+aws sts get-caller-identity
+aws events put-targets
+```
+
+1. Grab yourself a copy of this repo `git clone https://github.com/sarathpantala/AWS-IAM-KEY-DISABLER.git`
+2. Navigate into the projects `grunt` folder: run `cd AWS-IAM-KEY-DISABLER/grunt`
+3. Setup the Grunt task runner, e.g. install its dependencies: run `npm install`
+4. Update the custom configuration within the `/grunt/package.json` file:
+
+  * Update `key_disabler.aws.account_name` to contain name (metadata) of AWS account into which deployment will take place
+  * Update `key_disabler.aws.account_id` to contain AWS account into which deployment will take place
+  * Update `key_disabler.aws.region` to contain AWS region into which deployment will take place
+  * Update `--skipusers=sysuser1,sysuser2` to contain AWS region into which deployment will take place
+
+  **Note**: Can be overridden at deployment time using `grunt deployLambda --awsaccountname=BLAHCORP --awsaccountid=123456789012 --awsregion=us-east-1 --skipusers=sysuser1,sysuser2 --sesregion=us-east-1`
+
+  * Update `key_disabler.keystates.first_warning` and `key_disabler.keystates.last_warning` to the age that the key has to be in days to trigger an email warning.
+  * Update `key_disabler.keystates.expired` to the age in days when the key expires. At this age the key is disabled.
+  * Set `key_disabler.email.admin.enabled` to `True` if you want to send an email report to an administrator email address containing a full report of all IAM users and their Access Key status. Email delivery is performed via AWS SES (make sure that it has been configured correctly). Configure `key_disabler.email.admin.to` to be a valid email address.
+  * Set `key_disabler.email.user.enabled` to `True` if you want to send an individual email to each IAM user - containing the information about their Access Key status and whether a particular key is due to be expired or has been expired.
+
+    * Configure one of the following options:
+      * Set `key_disabler.email.user.emailaddressconfig.type` to `tag` for tag based email addresses - you also need to specify the **tag** name `key_disabler.email.user.emailaddressconfig.tagname` for this option.
+      * Set `key_disabler.email.user.emailaddressconfig.type` to `username` for **username** based email addresses
+        * Set `key_disabler.email.user.emailaddressconfig.tagname` to be the name of the tag on the user account that contains the user's email address
+        * Set `key_disabler.email.user.emailaddressconfig.reportmissingtag` to `True` to send an email to the administrator if the tag `key_disabler.email.user.emailaddressconfig.tagname` does NOT contain a valid email address
+
+  * Update `key_disabler.email.from` to be a valid email address.
+  * Update `key_disabler.lambda.schedule.expression` to be a valid cron job expression for when you want the Lambda function automatically triggered.
+
+  * Update `key_disabler.iam.skip_usernames` with a list of IAM usernames which should be skipped - used to specify special IAM accounts (service or system type accounts) which don't need key rotation. Can be overridden at deployment time using `grunt deployLambda --skipusers=sysuser1,sysuser2`
+
+5. From within the `/grunt` directory - run `grunt bumpup && grunt deployLambda` to bump your release version number and perform a build/deploy of the Lambda function to the selected region
+
+## Invoking the Lambda Function manually from the commandline using the AWSCLI
+
+This is useful for testing and performing adhoc runs
+
+Execute the lambda function by name, `AccessKeyRotation`, logging the output of the scan to a file called `scan.report.log`:
+
+`aws lambda invoke --function-name AccessKeyRotation scan.report.log --region us-east-1`
+```javascript
+{
+    "StatusCode": 200
+}
+```
+
+Use `jq` to render the contents of the `scan.report.log` to the console:
+
+`jq '.' scan.report.log`
+```javascript
+{
+  "reportdate": "2016-06-26 10:37:24.071091",
+  "users": [
+    {
+      "username": "BlahUser22",
+      "userid": "2",
+      "keys": []
+    },
+    {
+      "username": "LambdaFake1",
+      "userid": "3",
+       "keys": [
+        {
+          "age": 23,
+          "changed": false,
+          "state": "key is due to expire in 1 week (7 days)",
+          "accesskeyid": "**************DFG12"
+        },
+        {
+          "age": 296,
+          "changed": false,
+          "state": "key is already in an INACTIVE state",
+          "accesskeyid": "**************4ZASD"
+        }
+      ]
+    },
+    {
+      "username": "apiuser49",
+      "userid": "4",
+       "keys": [
+        {
+          "age": 30,
+          "changed": true,
+          "state": "key is now EXPIRED! Changing key to INACTIVE state",
+          "accesskeyid": "**************ER2E2"
+        },
+        {
+          "age": 107,
+          "changed": false,
+          "state": "key is already in an INACTIVE state",
+          "accesskeyid": "**************AWQ4K"
+        }
+      ]
+    },
+    {
+      "username": "UserEMRKinesis",
+      "userid": "5",
+       "keys": [
+        {
+          "age": 30,
+          "changed": false,
+          "state": "key is now EXPIRED! Changing key to INACTIVE state",
+          "accesskeyid": "**************MGB41A"
+        }
+      ]
+    },
+    {
+      "username": "CDN-Drupal",
+      "userid": "6",
+       "keys": [
+        {
+          "age": 10,
+          "changed": false,
+          "state": "key is still young",
+          "accesskeyid": "**************ZDSQ5A"
+        },
+        {
+          "age": 5,
+          "changed": false,
+          "state": "key is still young",
+          "accesskeyid": "**************E3ODA"
+        }
+      ]
+    },
+    {
+      "username": "ChocDonutUser1",
+      "userid": "7",
+       "keys": [
+        {
+          "age": 59,
+          "changed": false,
+          "state": "key is already in an INACTIVE state",
+          "accesskeyid": "**************CSA123"
+        }
+      ]
+    },
+    {
+      "username": "ChocDonut2",
+      "userid": "8",
+       "keys": [
+        {
+          "age": 60,
+          "changed": false,
+          "state": "key is already in an INACTIVE state",
+          "accesskeyid": "**************FDGD2"
+        }
+      ]
+    },
+      ]
+    }
+  ]
+}
+```
+
+## Additional configuration option
+
+* You can choose to set the message used for each warning and the final disabling by changing the values under `key_disabler.keystates.<state>.message`
+* You can change the length of masking under `key_disabler.mask_accesskey_length`. The access keys are 20 characters in length.
+
+## Troubleshooting
+
+This script is provided as is. We are happy to answer questions as time allows but can't give any promises.
+
+If things don't work ensure that:
+* You can authenticate successfully against AWS using the AWSCLI commandline tool
+* SES is not in sandbox mode and the sender domain has been verified
+* The selected region provides both Lambda and SES https://aws.amazon.com/about-aws/global-infrastructure/regional-product-services/
+
+## Bonus Points
+
+Once the Lambda Function has been successfully deployed - the following commands can be performed:
+
+1. `aws lambda list-functions`
+2. `openssl dgst -binary -sha256 ..\Releases\AccessKeyRotationPackage.1.0.18.zip | openssl base64`
+3. `aws lambda invoke --function-name AccessKeyRotation report.log --region us-east-1`
+4. `jq '.' report.log`
+5. `jq '.users[] | select(.username=="johndoe")' report.log`
+5. `jq '.' report.log | grep age | cut -d':' -f2 | sort -n`
+
+## Bonus Bonus Points
+
+1. `jq 'def maximal_by(f): (map(f) | max) as $mx | .[] | select(f == $mx); .users | maximal_by(.keys[].age)' report.log`
+2. `jq 'def minimal_by(f): (map(f) | min) as $mn | .[] | select(f == $mn); .users | minimal_by(.keys[].age)' report.log`
